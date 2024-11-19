b4687116ac67130e81658a3048d7a7910a715593
Pramudita <pbhattarai0129@gmail.com>
2024-11-19 07:23:04 +0545

    First Question Completed

---
+++ week1/basicFunction.dart
@@ -0,0 +1,12 @@
+//Question 1: Basic Function Definition
+//Write a Dart function named greetUser that takes a string parameter name and prints "Hello, [name]!".
+
+//function definition
+void greetUser(String name){
+  print("Hello, $name!");
+}
+
+//Example Usage
+void main(){
+  greetUser("Pramudita");
+}
