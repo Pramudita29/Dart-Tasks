eaaf455e4fc073747476ae51d262acccb4c1bf1b
Pramudita <pbhattarai0129@gmail.com>
2024-11-29 15:48:08 +0545

    Initial commit: main.dart and app.dart

--- week2/class_work/odd_even/bin/odd_even.dart
+++
@@ -1,5 +0,0 @@
-// import 'package:odd_even/odd_even.dart' as odd_even;
-
-// void main(List<String> arguments) {
-//   print('Hello world: ${odd_even.calculate()}!');
-// }
--- week2/class_work/odd_even/lib/repeat.dart
+++
@@ -1,9 +0,0 @@
-void main() {
-  printName("Pramudita Bhattarai", 100); 
-}
-
-void printName(String name, int times) {
-  for (int i = 1; i <= times; i++) {
-    print("$i. $name");
-  }
-}
